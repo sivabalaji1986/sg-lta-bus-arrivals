7a8666df21285578de484923c83a84a580167c61
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-03 02:45:33 +0800

    Created the SpringBootApplication

---
+++ src/main/java/com/hbs/lta/LTABusTimingApplication.java
@@ -0,0 +1,11 @@
+package com.hbs.lta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LTABusTimingApplication  {
+    public static void main(String[] args) {
+        SpringApplication.run(LTABusTimingApplication.class, args);
+    }
+}
