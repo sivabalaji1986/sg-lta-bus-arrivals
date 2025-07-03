c0a6ff962597a3f2353f57e79eeeef037aef6d1c
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-03 10:33:31 +0800

    Updated the schema definition of the fields with details

--- src/main/java/com/hbs/lta/model/LTANextBus.java
+++ src/main/java/com/hbs/lta/model/LTANextBus.java
@@ -2,9 +2,11 @@ package com.hbs.lta.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "Bus arrival timing details")
 public class LTANextBus {
 
     @JsonIgnore
@@ -15,6 +17,7 @@ public class LTANextBus {
     @JsonProperty("DestinationCode")
     private String DestinationCode;
 
+    @Schema(description = "Estimated arrival time in ISO 8601 format")
     @JsonProperty("EstimatedArrival")
     private String EstimatedArrival;
 
@@ -26,15 +29,31 @@ public class LTANextBus {
     @JsonProperty("Longitude")
     private String Longitude;
 
+    @Schema(description = "Visit number of the bus at this stop")
     @JsonProperty("VisitNumber")
     private String VisitNumber;
 
+    @Schema(description = """
+            Passenger load:
+            - SEA: Seats Available
+            - SDA: Standing Available
+            - LSD: Limited Standing
+            """)
     @JsonProperty("Load")
     private String Load;
 
+    @Schema(description = """
+            Bus feature:
+            - WAB: Wheelchair Accessible Bus
+            """)
     @JsonProperty("Feature")
     private String Feature;
 
+    @Schema(description = """
+            Bus type:
+            - SD: Single Deck
+            - DD: Double Deck
+            """)
     @JsonProperty("Type")
     private String Type;
 
